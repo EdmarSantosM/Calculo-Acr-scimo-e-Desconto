7a5868a419a0d6e5b0d1f27a0ab898461b898d62
EdmarSantosM <santosem4@hotmail.com>
2023-05-01 11:46:25 -0300

    Update Program.cs

--- AcrescimoDescontoNoValor/Program.cs
+++ AcrescimoDescontoNoValor/Program.cs
@@ -11,9 +11,17 @@
         Console.Write(" Digite a descrição do produto : ");
         descricao = Console.ReadLine();
 
+
         /* Para as variáveis valorCusto, percentual é necessário realizar a conversão utlizando Convert.ToDecimal devido  
          o valor  recebido pelo ReadLine por padrão vem como strig*/
 
+         decimal valorCusto = 0;
+         Console.Write(" Digite o valor de custo do produto informado R$");
+         valorCusto = Convert.ToDecimal(Console.ReadLine());
+
+         decimal percentual = 0;
+         Console.Write($" Digite o percentual da loja {percentual}%");
+         percentual = Convert.ToDecimal(Console.ReadLine());
          
     }
  }
