f9b0a26b5835034ce04b73a2d1298e61aff2c62f
EdmarSantosM <santosem4@hotmail.com>
2023-05-01 11:28:41 -0300

    Update Program.cs
    ..

--- AcrescimoDescontoNoValor/Program.cs
+++ AcrescimoDescontoNoValor/Program.cs
@@ -1,8 +1,19 @@
 ﻿ public class Program
  {
-    
+
     static void Main(string[]args)
     {
+        System.Console.WriteLine("\tDigite as informações do produto ");
+        System.Console.WriteLine();
 
+        // Definindo as variáveis que receberam  os valores recebido com método ReadLine
+        string descricao;
+        Console.Write(" Digite a descrição do produto : ");
+        descricao = Console.ReadLine();
+
+        /* Para as variáveis valorCusto, percentual é necessário realizar a conversão utlizando Convert.ToDecimal devido  
+         o valor  recebido pelo ReadLine por padrão vem como strig*/
+
+         
     }
  }
